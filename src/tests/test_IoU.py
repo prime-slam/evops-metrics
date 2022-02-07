68ece3b20e35f8d644f2d72d6c6832203364b8f0
pmokeev <pav3l.mokeev@gmail.com>
2022-02-07 15:57:58 +0300

    Added some more tests

--- src/tests/test_IoU.py
+++ src/tests/test_IoU.py
@@ -44,3 +44,31 @@ def test_cumulative_null_iou_result():
             point_cloud, point_cloud_predicted, point_cloud_gt
         )
     )
+
+
+def test_null_iou_result():
+    point_cloud = np.empty((0, 3), np.float64)
+    point_cloud_predicted = np.array([1, 2, 3, 4])
+    point_cloud_gt = np.array([5, 6, 7, 8])
+
+    iouBenchmark = IoUBenchmark()
+
+    assert 0 == pytest.approx(
+        iouBenchmark.calculate_metric(
+            point_cloud, point_cloud_predicted, point_cloud_gt
+        )
+    )
+
+
+def test_full_iou_result():
+    point_cloud = np.empty((0, 3), np.float64)
+    point_cloud_predicted = np.array([1, 2, 3, 4])
+    point_cloud_gt = np.array([1, 2, 3, 4])
+
+    iouBenchmark = IoUBenchmark()
+
+    assert 1 == pytest.approx(
+        iouBenchmark.calculate_metric(
+            point_cloud, point_cloud_predicted, point_cloud_gt
+        )
+    )
