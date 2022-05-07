Fixed default metrics calculation

src/evops/metrics/DefaultBenchmark.py
@@ -24,7 +24,7 @@ def __precision(
     gt_labels: NDArray[Any, np.int32],
     statistic_func: Callable[
         [NDArray[Any, np.int32], NDArray[Any, np.int32]],
-        tuple[np.int32, np.int32, np.int32],
+        np.int32,
     ],
 ) -> np.float64:
     pred_labels = np.delete(
@@ -35,9 +35,14 @@ def __precision(
         pred_labels.size != 0
     ), "Incorrect predicted label array values, most likely no labels other than UNSEGMENTED_LABEL"
 
-    true_positive, _, _ = statistic_func(pred_labels, gt_labels)
+    true_positive = statistic_func(pred_labels, gt_labels)
+    pred_planes = np.unique(pred_labels)
+    pred_planes = np.delete(
+        pred_planes,
+        np.where(pred_planes == evops.metrics.constants.UNSEGMENTED_LABEL)[0],
+    )
 
-    return true_positive / pred_labels.size
+    return true_positive / pred_planes.size
 
 
 def __recall(
@@ -45,7 +50,7 @@ def __recall(
     gt_labels: NDArray[Any, np.int32],
     statistic_func: Callable[
         [NDArray[Any, np.int32], NDArray[Any, np.int32]],
-        tuple[np.int32, np.int32, np.int32],
+        np.int32,
     ],
 ) -> np.float64:
     gt_labels = np.delete(
@@ -55,9 +60,14 @@ def __recall(
         gt_labels.size != 0
     ), "Incorrect ground truth label array values, most likely no labels other than UNSEGMENTED_LABEL"
 
-    true_positive, _, _ = statistic_func(pred_labels, gt_labels)
+    true_positive = statistic_func(pred_labels, gt_labels)
+    gt_planes = np.unique(gt_labels)
+    gt_planes = np.delete(
+        gt_planes,
+        np.where(gt_planes == evops.metrics.constants.UNSEGMENTED_LABEL)[0],
+    )
 
-    return true_positive / gt_labels.size
+    return true_positive / gt_planes.size
 
 
 def __fScore(
@@ -65,7 +75,7 @@ def __fScore(
     gt_labels: NDArray[Any, np.int32],
     statistic_func: Callable[
         [NDArray[Any, np.int32], NDArray[Any, np.int32]],
-        tuple[np.int32, np.int32, np.int32],
+        np.int32,
     ],
 ) -> np.float64:
     precision = __precision(pred_labels, gt_labels, statistic_func)

src/evops/utils/IoUOverlap.py
@@ -23,7 +23,7 @@ from evops.metrics.IoUBenchmark import __iou
 def __iou_overlap(
     pred_labels: NDArray[Any, np.int32],
     gt_labels: NDArray[Any, np.int32],
-) -> (np.int32, np.int32, np.int32):
+) -> np.int32:
     """
     :param pred_labels: labels of points corresponding to segmented planes
     :param gt_labels: indices of points corresponding to ground truth planes
@@ -40,6 +40,7 @@ def __iou_overlap(
         unique_pred_labels,
         np.where(unique_pred_labels == evops.metrics.constants.UNSEGMENTED_LABEL)[0],
     )
+    pred_used = set()
 
     for gt_label in unique_gt_labels:
         is_already_true_positive = False
@@ -51,9 +52,11 @@ def __iou_overlap(
 
             if (
                 IoU_value >= evops.metrics.constants.IOU_THRESHOLD
+                and pred_label not in pred_used
                 and not is_already_true_positive
             ):
                 true_positive += 1
                 is_already_true_positive = True
+                pred_used.add(pred_label)
 
-    return true_positive, 0, 0
+    return true_positive

tests/test_default_metrics.py
@@ -10,6 +10,9 @@ from evops.metrics import (
 
 
 def test_full_precision_iou_statistics_result():
+    evops.metrics.constants.IOU_THRESHOLD = 0.75
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     pred_labels = np.array([1, 2, 3, 4])
     gt_labels = np.array([1, 2, 3, 4])
 
@@ -17,6 +20,9 @@ def test_full_precision_iou_statistics_result():
 
 
 def test_null_precision_iou_statistics_result():
+    evops.metrics.constants.IOU_THRESHOLD = 0.75
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     pred_labels = np.array([1, 1, 1, 1])
     gt_labels = np.array([0, 0, 0, 0])
 
@@ -24,13 +30,19 @@ def test_null_precision_iou_statistics_result():
 
 
 def test_half_precision_iou_statistics_result():
-    pred_labels = np.array([1, 1, 3, 4])
+    evops.metrics.constants.IOU_THRESHOLD = 0.75
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
+    pred_labels = np.array([1, 1, 3, 3])
     gt_labels = np.array([2, 2, 0, 3])
 
     assert 0.5 == pytest.approx(precision(pred_labels, gt_labels, "iou"))
 
 
 def test_precision_iou_statistics_assert():
+    evops.metrics.constants.IOU_THRESHOLD = 0.75
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     with pytest.raises(AssertionError) as excinfo:
         pred_labels = np.array([0, 0, 0, 0])
         gt_labels = np.array([1, 1, 1, 1])
@@ -44,6 +56,9 @@ def test_precision_iou_statistics_assert():
 
 
 def test_null_recall_iou_statistics_result():
+    evops.metrics.constants.IOU_THRESHOLD = 0.75
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     pred_labels = np.array([1, 1, 1, 1])
     gt_labels = np.array([1, 2, 3, 4])
 
@@ -51,20 +66,39 @@ def test_null_recall_iou_statistics_result():
 
 
 def test_full_recall_iou_statistics_result():
+    evops.metrics.constants.IOU_THRESHOLD = 0.75
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     pred_labels = np.array([1, 2, 3, 4])
     gt_labels = np.array([5, 6, 7, 8])
 
     assert 1 == pytest.approx(recall(pred_labels, gt_labels, "iou"))
 
 
-def test_half_recall_iou_statistics_result():
+def test_full_recall_with_two_planes_iou_statistics_result():
+    evops.metrics.constants.IOU_THRESHOLD = 0.75
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     pred_labels = np.array([1, 1, 2, 2])
     gt_labels = np.array([2, 2, 3, 3])
+
+    assert 1 == pytest.approx(recall(pred_labels, gt_labels, "iou"))
+
+
+def test_half_recall_iou_statistics_result():
+    evops.metrics.constants.IOU_THRESHOLD = 0.75
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
+    pred_labels = np.array([2, 2, 0, 3])
+    gt_labels = np.array([1, 1, 3, 3])
 
     assert 0.5 == pytest.approx(recall(pred_labels, gt_labels, "iou"))
 
 
 def test_recall_iou_statistics_assert():
+    evops.metrics.constants.IOU_THRESHOLD = 0.75
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     with pytest.raises(AssertionError) as excinfo:
         pred_labels = np.array([1, 1, 1, 1])
         gt_labels = np.array([0, 0, 0, 0])
@@ -78,44 +112,50 @@ def test_recall_iou_statistics_assert():
 
 
 def test_full_fScore_iou_statistics_result():
+    evops.metrics.constants.IOU_THRESHOLD = 0.75
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     pred_labels = np.array([1, 2, 3, 4])
     gt_labels = np.array([5, 6, 7, 8])
 
     assert 1 == pytest.approx(fScore(pred_labels, gt_labels, "iou"))
 
 
-def test_half_fScore_iou_statistics_result():
+def test_almost_half_fScore_iou_statistics_result():
+    evops.metrics.constants.IOU_THRESHOLD = 0.75
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     pred_labels = np.array([1, 1, 2, 3])
     gt_labels = np.array([4, 5, 6, 7])
 
-    assert 0.5 == pytest.approx(fScore(pred_labels, gt_labels, "iou"))
+    assert 0.57 == pytest.approx(fScore(pred_labels, gt_labels, "iou"), 0.01)
 
 
 def test_precision_real_data_iou_statistics():
-    evops.metrics.constants.IOU_THRESHOLD = 0.1
+    evops.metrics.constants.IOU_THRESHOLD = 0.5
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     pred_labels = np.load("tests/data/pred_0.npy")
     gt_labels = np.load("tests/data/gt_0.npy")
 
-    assert 0.0000201 == pytest.approx(
-        float("{:.7f}".format(precision(pred_labels, gt_labels, "iou"))), 0.00001
-    )
+    assert 0.2 == pytest.approx(precision(pred_labels, gt_labels, "iou"), 0.01)
 
 
 def test_recall_real_data_iou_statistics():
-    evops.metrics.constants.IOU_THRESHOLD = 0.1
+    evops.metrics.constants.IOU_THRESHOLD = 0.5
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     pred_labels = np.load("tests/data/pred_0.npy")
     gt_labels = np.load("tests/data/gt_0.npy")
 
-    assert 0.0000147 == pytest.approx(
-        float("{:.7f}".format(recall(pred_labels, gt_labels, "iou"))), 0.00001
-    )
+    assert 0.059 == pytest.approx(recall(pred_labels, gt_labels, "iou"), 0.01)
 
 
 def test_fScore_real_data_iou_statistics():
-    evops.metrics.constants.IOU_THRESHOLD = 0.1
+    evops.metrics.constants.IOU_THRESHOLD = 0.5
+    evops.metrics.constants.UNSEGMENTED_LABEL = 0
+
     pred_labels = np.load("tests/data/pred_0.npy")
     gt_labels = np.load("tests/data/gt_0.npy")
 
-    assert 1.7e-05 == pytest.approx(
-        float("{:.7f}".format(fScore(pred_labels, gt_labels, "iou"))), 0.00001
-    )
+    assert 0.09 == pytest.approx(fScore(pred_labels, gt_labels, "iou"), 0.01)