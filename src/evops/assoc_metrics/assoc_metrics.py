9d697c4a1cb8603b3f7a326754d639aa6e375f80
vnmsklnk <st086866@studet.spbu.ru>
2022-08-17 10:05:39 +0300

    Metrics interface with groundtruth

--- src/evops/assoc_metrics/assoc_metrics.py
+++ src/evops/assoc_metrics/assoc_metrics.py
@@ -1,39 +1,41 @@
-from typing import Dict, Optional, Tuple
+from typing import Optional
 
 
 def quantitative_planes(
-    assoc_dict: Dict[int, Optional[int]], planes_from_previous_frame: set[int]
+    assoc_dict: dict[int, Optional[int]], gt_assoc_dict: dict[int, Optional[int]]
 ) -> float:
     """
     Metric for calculating the ratio of number correctly associated planes to their total number
     :param assoc_dict: dictionary in
     {ID of plane from current frame: ID of plane from previous frame} format
-    :param planes_from_previous_frame: set of planes from previous frame
+    :param gt_assoc_dict: groundtruth association dictionary in
+    {ID of plane from current frame: ID of plane from previous frame} format
     :return: metric result
     """
     right = 0
     for cur, prev in assoc_dict.items():
-        if (cur == prev) or (prev is None and cur not in planes_from_previous_frame):
+        if gt_assoc_dict[cur] == prev:
             right += 1
     return right / len(assoc_dict)
 
 
 def quantitative_points(
-    assoc_dict_with_length: Dict[Tuple[int, int], Optional[int]],
-    planes_from_previous_frame: set,
+    assoc_dict_with_length: dict[tuple[int, int], Optional[int]],
+    gt_assoc_dict: dict[int, Optional[int]],
 ) -> float:
     """
     Metric for calculating the ratio of number correctly associated points to their total number
     :param assoc_dict_with_length: dictionary in
     {(ID of plane from current frame, number of points in plane):
     ID of plane from previous frame} format
-    :param planes_from_previous_frame: set of planes from previous frame
+    :param gt_assoc_dict: groundtruth association dictionary in
+    {ID of plane from current frame: ID of plane from previous frame} format
     :return: metric result
     """
     right = 0
     all_points = 0
     for (cur, cur_length), prev in assoc_dict_with_length.items():
         all_points += cur_length
-        if (cur == prev) or (prev is None and cur not in planes_from_previous_frame):
+        if gt_assoc_dict[cur] == prev:
             right += cur_length
     return right / all_points
--- tests/test_assoc_planes.py
+++ tests/test_assoc_planes.py
@@ -3,28 +3,28 @@ from evops.assoc_metrics import quantitative_planes
 
 
 def test_full_planes_result():
-    results = {1: 1, 2: 2, 4: 4, 5: 5}
-    prev_planes = {1, 2, 4, 5}
+    results = {1: 2, 2: 1, 4: 4, 5: 5}
+    groundtruth = {1: 2, 2: 1, 4: 4, 5: 5}
 
-    assert 1 == pytest.approx(quantitative_planes(results, prev_planes))
+    assert 1 == pytest.approx(quantitative_planes(results, groundtruth))
 
 
 def test_half_planes_result():
     results = {1: 1, 2: 4, 4: 2, 5: 5}
-    prev_planes = {1, 2, 4, 5}
+    groundtruth = {1: 1, 2: 2, 4: 4, 5: 5}
 
-    assert 0.5 == pytest.approx(quantitative_planes(results, prev_planes))
+    assert 0.5 == pytest.approx(quantitative_planes(results, groundtruth))
 
 
 def test_full_planes_result_with_none():
     results = {1: 1, 2: None, 3: 3}
-    prev_planes = {1, 3}
+    groundtruth = {1: 1, 2: None, 3: 3}
 
-    assert 1 == pytest.approx(quantitative_planes(results, prev_planes))
+    assert 1 == pytest.approx(quantitative_planes(results, groundtruth))
 
 
 def test_none_planes_result():
-    results = {1: 3, 2: None, 3: 1}
-    prev_planes = {1, 2, 3}
+    results = {1: 1, 2: None, 3: 3}
+    groundtruth = {1: 3, 2: 4, 3: 1}
 
-    assert 0 == pytest.approx(quantitative_planes(results, prev_planes))
+    assert 0 == pytest.approx(quantitative_planes(results, groundtruth))
