07e95378e3f8b1e88117bf87d46cd8a450b63450
vnmsklnk <st086866@studet.spbu.ru>
2022-08-17 10:05:39 +0300

    Metrics interface with groundtruth

--- tests/test_assoc__points.py
+++ tests/test_assoc__points.py
@@ -3,28 +3,28 @@ from evops.assoc_metrics import quantitative_points
 
 
 def test_full_points_result():
-    results = {(1, 100): 1, (2, 50): 2, (4, 200): 4}
-    prev_planes = {1, 2, 4}
+    results = {(1, 100): 2, (2, 50): 1, (4, 200): 4}
+    groundtruth = {1: 2, 2: 1, 4: 4}
 
-    assert 1 == pytest.approx(quantitative_points(results, prev_planes))
+    assert 1 == pytest.approx(quantitative_points(results, groundtruth))
 
 
 def test_half_points_result():
-    results = {(1, 100): 1, (2, 200): 4, (3, 100): 3}
-    prev_planes = {1, 2, 3, 4}
+    results = {(1, 100): 2, (2, 200): 1, (3, 100): 5}
+    groundtruth = {1: 2, 2: 3, 3: 5}
 
-    assert 0.5 == pytest.approx(quantitative_points(results, prev_planes))
+    assert 0.5 == pytest.approx(quantitative_points(results, groundtruth))
 
 
 def test_full_points_result_with_none():
     results = {(1, 10): 1, (2, 30): None, (3, 50): 3}
-    prev_planes = {1, 3}
+    groundtruth = {1: 1, 2: None, 3: 3}
 
-    assert 1 == pytest.approx(quantitative_points(results, prev_planes))
+    assert 1 == pytest.approx(quantitative_points(results, groundtruth))
 
 
 def test_none_points_result():
-    results = {(1, 10): 3, (2, 5): None, (3, 1000): 1}
-    prev_planes = {1, 2, 3}
+    results = {(1, 10): 1, (2, 5): None, (3, 1000): 3}
+    groundtruth = {1: 3, 2: 5, 3: 1}
 
-    assert 0 == pytest.approx(quantitative_points(results, prev_planes))
+    assert 0 == pytest.approx(quantitative_points(results, groundtruth))
