fe2f73ca5b4f6f2477c6588ab942e78bd03970d5
vnmsklnk <st086866@student.spbu.ru>
2022-08-25 22:43:52 +0300

    Fixes

--- src/evops/assoc_metrics/assoc_metrics.py
+++ src/evops/assoc_metrics/assoc_metrics.py
@@ -1,34 +1,20 @@
 import numpy as np
-from typing import Optional, Callable
-from numpy.typing import NDArray
-from matching import match_labels_with_groundtruth
+from typing import Optional, Callable, Any
+from nptyping import NDArray
+from evops.assoc_metrics.matching import match_labels_with_groundtruth
 
 
 def quantitative_planes(
-    assoc_dict: dict[int, Optional[int]],
-    labels_cur: NDArray[np.uint8],
-    labels_prev: NDArray[np.uint8],
-    labeled_image_cur: NDArray[np.uint8],
-    labeled_image_prev: NDArray[np.uint8],
-    matcher: Callable[
-        [NDArray[np.uint8], NDArray[np.uint8], NDArray[np.uint8], NDArray[np.uint8]],
-        dict[int, Optional[int]],
-    ] = match_labels_with_groundtruth,
+    assoc_dict: dict[int, Optional[int]], gt_assoc_dict: dict[int, Optional[int]]
 ) -> float:
     """
     Metric for calculating the ratio of number correctly associated planes to their total number
     :param assoc_dict: dictionary in
     {ID of plane from current frame: ID of plane from previous frame} format
-    :param labels_cur: labels from current frame
-    :param labels_prev: labels from previous frame
-    :param labeled_image_cur: current labeled groundtruth frame
-    :param labeled_image_prev: previous labeled groundtruth frame
-    :param matcher: func that matches labels from two frames using groundtruth
+    :param gt_assoc_dict: groundtruth association dictionary in
+    {ID of plane from current frame: ID of plane from previous frame} format
     :return: metric result
     """
-    gt_assoc_dict = matcher(
-        labels_cur, labels_prev, labeled_image_cur, labeled_image_prev
-    )
     right = 0
     for cur, prev in assoc_dict.items():
         if gt_assoc_dict[cur] == prev:
@@ -38,12 +24,73 @@ def quantitative_planes(
 
 def quantitative_points(
     assoc_dict: dict[int, Optional[int]],
-    labels_cur: NDArray[np.uint8],
-    labels_prev: NDArray[np.uint8],
-    labeled_image_cur: NDArray[np.uint8],
-    labeled_image_prev: NDArray[np.uint8],
+    planes_length: dict[int, int],
+    gt_assoc_dict: dict[int, Optional[int]],
+) -> float:
+    """
+    Metric for calculating the ratio of number correctly associated points to their total number
+    :param assoc_dict: dictionary in
+    {ID of plane from current frame: ID of plane from previous frame} format
+    :param planes_length: dictionary in
+    {ID of plane from current frame: number of points} format
+    :param gt_assoc_dict: groundtruth association dictionary in
+    {ID of plane from current frame: ID of plane from previous frame} format
+    :return: metric result
+    """
+    right = 0
+    all_points = 0
+    for cur, prev in assoc_dict.items():
+        cur_length = planes_length[cur]
+        all_points += cur_length
+        if gt_assoc_dict[cur] == prev:
+            right += cur_length
+    return right / all_points
+
+
+def quantitative_planes_with_matching(
+    assoc_dict: dict[int, Optional[int]],
+    labels_cur: NDArray[(Any, Any), np.uint8],
+    labels_prev: NDArray[(Any, Any), np.uint8],
+    gt_labels_cur: NDArray[(Any, Any, 3), np.uint8],
+    gt_labels_prev: NDArray[(Any, Any, 3), np.uint8],
     matcher: Callable[
-        [NDArray[np.uint8], NDArray[np.uint8], NDArray[np.uint8], NDArray[np.uint8]],
+        [
+            NDArray[(Any, Any), np.uint8],
+            NDArray[(Any, Any), np.uint8],
+            NDArray[(Any, Any, 3), np.uint8],
+            NDArray[(Any, Any, 3), np.uint8],
+        ],
+        dict[int, Optional[int]],
+    ] = match_labels_with_groundtruth,
+) -> float:
+    """
+    Metric for calculating the ratio of number correctly associated planes to their total number
+    :param assoc_dict: dictionary in
+    {ID of plane from current frame: ID of plane from previous frame} format
+    :param labels_cur: labels from current frame
+    :param labels_prev: labels from previous frame
+    :param gt_labels_cur: current labeled groundtruth frame
+    :param gt_labels_prev: previous labeled groundtruth frame
+    :param matcher: func that matches labels from two frames using groundtruth
+    :return: metric result
+    """
+    gt_assoc_dict = matcher(labels_cur, labels_prev, gt_labels_cur, gt_labels_prev)
+    return quantitative_planes(assoc_dict, gt_assoc_dict)
+
+
+def quantitative_points_with_matching(
+    assoc_dict: dict[int, Optional[int]],
+    labels_cur: NDArray[(Any, Any), np.uint8],
+    labels_prev: NDArray[(Any, Any), np.uint8],
+    gt_labels_cur: NDArray[(Any, Any, 3), np.uint8],
+    gt_labels_prev: NDArray[(Any, Any, 3), np.uint8],
+    matcher: Callable[
+        [
+            NDArray[(Any, Any), np.uint8],
+            NDArray[(Any, Any), np.uint8],
+            NDArray[(Any, Any, 3), np.uint8],
+            NDArray[(Any, Any, 3), np.uint8],
+        ],
         dict[int, Optional[int]],
     ] = match_labels_with_groundtruth,
 ) -> float:
@@ -54,19 +101,13 @@ def quantitative_points(
     ID of plane from previous frame} format
     :param labels_cur: labels from current frame
     :param labels_prev: labels from previous frame
-    :param labeled_image_cur: current labeled groundtruth frame
-    :param labeled_image_prev: previous labeled groundtruth frame
+    :param gt_labels_cur: current labeled groundtruth frame
+    :param gt_labels_prev: previous labeled groundtruth frame
     :param matcher: func that matches labels from two frames using groundtruth
     :return: metric result
     """
-    gt_assoc_dict = matcher(
-        labels_cur, labels_prev, labeled_image_cur, labeled_image_prev
-    )
-    right = 0
-    all_points = 0
-    for cur, prev in assoc_dict.items():
-        cur_length = len(np.where(labels_cur == cur)[0])
-        all_points += cur_length
-        if gt_assoc_dict[cur] == prev:
-            right += cur_length
-    return right / all_points
+    gt_assoc_dict = matcher(labels_cur, labels_prev, gt_labels_cur, gt_labels_prev)
+    planes_length = dict()
+    for cur, _ in assoc_dict.items():
+        planes_length[cur] = len(np.where(labels_cur == cur)[0])
+    return quantitative_points(assoc_dict, planes_length, gt_assoc_dict)
